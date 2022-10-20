41fa2d2d5964393e607dc6c41388197ad1ab4b75
ArcosJuan <arcos0juan@gmail.com>
2022-10-20 23:23:05 +0000

    Añadir compatibilidad con Windows y Linux en scraper.

--- src/scraper/scrap_tool.py
+++ src/scraper/scrap_tool.py
@@ -1,22 +1,29 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-import time, sys, os
-
+import time, sys, os, platform
 
 class ScrapTool():
      @staticmethod
      def create_driver(url: str):
+          script_dir = os.path.dirname(__file__)
           driver = None
+
+          # Depending on the os, the initialization of the driver changes.
           if sys.platform == "win32" or sys.platform == "win64":
                options = Options()
                options.binary_location = r'C:\\Program Files\\Mozilla Firefox\\firefox.exe'
 
-               script_dir = os.path.dirname(__file__)
-               driver = webdriver.Firefox(executable_path=f"{script_dir}\\geckodriver.exe", options=options)
+               driver = webdriver.Firefox(executable_path=f"{script_dir}\\geckodrivers\\geckodriver.exe", options=options)
           elif sys.platform == "linux":
-               driver = webdriver.Firefox(executable_path='geckodriver')
+               if platform.freedesktop_os_release()['ID'] == 'linuxmint':
+                    driver = webdriver.Firefox(executable_path=f'{script_dir}/geckodriver')
+               else:
+                    driver = webdriver.Firefox(executable_path=f'geckodriver')
 
-          assert driver != None, "Driver was not initialized."
+          assert driver != None, "Driver was not initialized.\n" \
+               + f"Maybe the os: {sys.platform}, or the distro: " \
+               + f"{platform.freedesktop_os_release()['NAME']} is not supported."
+
           driver.get(url)
           return driver
 
